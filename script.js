9a0d0f112285c1b41f5a381ce801c9c52461d011
salvatore vitiello <marina.iacomino@gmail.com>
2024-02-05 20:40:47 +0000

    function to write password on display and event listener created

--- script.js
+++ script.js
@@ -145,4 +145,15 @@ function generatePassword() {
   return passChar;
 }
 
+// Write password to the #password input
 
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
